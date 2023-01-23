f45c9b88a324b396c5ffc4d17b21045780791728
shinjith-dev <shinjithpallikkara@gmail.com>
2023-01-23 19:46:03 +0530

    auth update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require("http");
 const app = require("./app");
 const { PORT } = require("./config");
+const { authorize } = require("./startSheet");
+
+authorize()
 
 const server = http.createServer(app);
 
